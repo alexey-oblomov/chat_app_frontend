faa7ede0596f50401470046547174791ff10d8e8
Alexey Oblomov <alexey.oblomov.1981@gmail.com>
2020-08-29 23:13:26 +0300

    add message component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import {Message} from 'components';
+
 import './Home.scss';
 
 const Auth = () => (
   <section className="home">
-    <h1>Hello</h1>
+    <Message
+      avatar="https://vk.com/images/camera_100.png?ava=1"
+      text="Привет! Как делa. HELLO"
+      date={new Date('Sat Aug 29 2020 21:19:07')}
+    ></Message>
+    <Message
+      avatar="https://vk.com/images/camera_100.png?ava=1"
+      text="Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло! Да-да. Хелло!"
+      date={new Date('Sat Aug 29 2020 11:19:07')}
+      isMe={true}
+      isReaded={false}
+    ></Message>
   </section>
 );
 
