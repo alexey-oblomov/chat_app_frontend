6e43bb553def2c51998f9e620c7c79dc88f0e086
Alexey Oblomov <alexey.oblomov.1981@gmail.com>
2020-08-29 17:17:54 +0300

    add auth form

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,22 +1,64 @@
 import React from 'react';
+import {Form, Input} from 'antd';
 import {Button, Block} from 'components';
+import {UserOutlined, LockOutlined} from '@ant-design/icons';
 
 import './Auth.scss';
 
-const Auth = () => (
-  <section className="auth">
-    <div className="auth__content">
-      <div className="auth__top">
-        <h2>Войти в аккаунт</h2>
-        <p>Пожалуйста, войдите в свой аккаунт</p>
+const Auth = () => {
+  const onFinish = values => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <section className="auth">
+      <div className="auth__content">
+        <div className="auth__top">
+          <h2>Войти в аккаунт</h2>
+          <p>Пожалуйста, войдите в свой аккаунт</p>
+        </div>
+        <Block>
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{remember: true}}
+            onFinish={onFinish}
+          >
+            <Form.Item
+              name="username"
+              rules={[{required: true, message: 'Please input your Username!'}]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Имя пользователя"
+                size="large"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[{required: true, message: 'Please input your Password!'}]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Пароль"
+                size="large"
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" size="large">
+                Войти в аккаунт
+              </Button>
+            </Form.Item>
+            <a className="auth__register-link" href="http://localhost:3000/">
+              Зарегистрироваться
+            </a>
+          </Form>
+        </Block>
       </div>
-      <Block>
-        <Button type="primary" size="large">
-          Кнопка!
-        </Button>
-      </Block>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Auth;
