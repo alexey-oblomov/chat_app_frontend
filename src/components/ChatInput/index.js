cc42f59ee2d5f0003d2bf4fb773c7581298194b5
Alexey Oblomov <alexey.oblomov.1981@gmail.com>
2020-08-31 21:36:39 +0300

    add change icon sound/send in ChatInput, fix ChatInput

--- src/components/ChatInput/index.js
+++ src/components/ChatInput/index.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {SmileOutlined, CameraOutlined, AudioOutlined, SendOutlined} from '@ant-design/icons';
-import {Input} from 'antd';
+import {Input, Button} from 'antd';
 
 import './ChatInput.scss';
 
-const ChatInput = props => (
-  <div className="chat-input">
-    <div className="chat-input__smile-btn">
-      <SmileOutlined />
+const ChatInput = props => {
+  const [value, setValue] = useState('');
+
+  return (
+    <div className="chat-input">
+      <div className="chat-input__smile-btn">
+        <Button shape="circle" icon={<SmileOutlined />} />
+      </div>
+      <Input
+        onChange={e => setValue(e.target.value)}
+        size="large"
+        placeholder="Введите текст сообщения"
+      />
+      <div className="chat-input__actions">
+        <Button shape="circle" icon={<CameraOutlined />} />
+
+        {value ? (
+          <Button shape="circle" icon={<SendOutlined />} />
+        ) : (
+          <Button shape="circle" icon={<AudioOutlined />} />
+        )}
+      </div>
     </div>
-    <Input size="large" placeholder="Введите текст сообщения" />
-    <div className="chat-input__actions">
-      <CameraOutlined />
-      <AudioOutlined />
-      <SendOutlined />
-    </div>
-  </div>
-);
+  );
+};
 
 ChatInput.propTypes = {
   className: PropTypes.string,
