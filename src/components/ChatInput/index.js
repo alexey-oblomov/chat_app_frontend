923c081893ff842764eeb1a7701abb6f278afb3c
Alexey Oblomov <alexey.oblomov.1981@gmail.com>
2020-09-02 12:26:53 +0300

    add emoji

--- src/components/ChatInput/index.js
+++ src/components/ChatInput/index.js
@@ -3,16 +3,28 @@ import PropTypes from 'prop-types';
 import {SmileOutlined, CameraOutlined, AudioOutlined, SendOutlined} from '@ant-design/icons';
 import {Input, Button} from 'antd';
 import {UploadField} from '@navjobs/upload';
+import {Picker} from 'emoji-mart';
 
 import './ChatInput.scss';
 
 const ChatInput = props => {
   const [value, setValue] = useState('');
+  const [emojiPickerVisible, setShowEmojiPicker] = useState(false);
+
+  const toggleEmojiPicker = () => {
+    setShowEmojiPicker(!emojiPickerVisible);
+  };
 
   return (
     <div className="chat-input">
       <div className="chat-input__smile-btn">
-        <Button shape="circle" icon={<SmileOutlined />} />
+        {emojiPickerVisible && (
+          <div className="chat-input__emoji-picker">
+            <Picker set="apple" />
+          </div>
+        )}
+
+        <Button onClick={toggleEmojiPicker} shape="circle" icon={<SmileOutlined />} />
       </div>
       <Input
         onChange={e => setValue(e.target.value)}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
-
 import {Provider} from 'react-redux';
 import store from './redux/store.js';
 
-import './styles/index.scss';
 import App from './App';
 
 import './styles/index.scss';
+import 'emoji-mart/css/emoji-mart.css';
+
 ReactDOM.render(
   <Provider store={store}>
     <Router>
