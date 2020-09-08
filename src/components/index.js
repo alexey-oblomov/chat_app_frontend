08ed5a8864d55634e1a9e4a7ac425865ad78e6bf
Alexey Oblomov <alexey.oblomov.1981@gmail.com>
2020-09-08 15:25:19 +0300

    add sidebar

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
+export {default as Sidebar} from './Sidebar';
 export {default as Button} from './Button';
 export {default as Block} from './Block';
 export {default as Message} from './Message';
+export {default as Messages} from './Messages';
 export {default as DialogItem} from './DialogItem';
 export {default as Time} from './Time';
 export {default as IconReaded} from './IconReaded';
@@ -8,5 +10,4 @@ export {default as Dialogs} from './Dialogs';
 export {default as Avatar} from './Avatar';
 export {default as Status} from './Status';
 export {default as ChatInput} from './ChatInput';
-export {default as Messages} from './Messages';
 export {default as FormField} from './FormField';
