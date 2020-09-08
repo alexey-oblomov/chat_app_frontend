2e4d1bac83161249b2d9c2d7d3aa28d6e80e75dc
Alexey Oblomov <alexey.oblomov.1981@gmail.com>
2020-09-08 11:51:26 +0300

    update chat input

--- src/components/ChatInput/index.js
+++ src/components/ChatInput/index.js
@@ -1,58 +1,99 @@
-import React, {useState} from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
-import {SmileOutlined, CameraOutlined, AudioOutlined, SendOutlined} from '@ant-design/icons';
-import {Input, Button} from 'antd';
+import {Button, Input} from 'antd';
 import {UploadField} from '@navjobs/upload';
 import {Picker} from 'emoji-mart';
 
+import {UploadFiles} from 'components';
+
 import './ChatInput.scss';
 
-const ChatInput = props => {
-  const [value, setValue] = useState('');
-  const [emojiPickerVisible, setShowEmojiPicker] = useState(false);
+const {TextArea} = Input;
 
-  const toggleEmojiPicker = () => {
-    setShowEmojiPicker(!emojiPickerVisible);
-  };
+const ChatInput = props => {
+  const {
+    emojiPickerVisible,
+    value,
+    addEmoji,
+    setValue,
+    handleSendMessage,
+    sendMessage,
+    toggleEmojiPicker,
+    attachments,
+    onSelectFiles,
+    isRecording,
+    onRecord,
+    onHideRecording,
+    removeAttachment,
+    isLoading,
+  } = props;
 
   return (
-    <div className="chat-input">
-      <div className="chat-input__smile-btn">
-        {emojiPickerVisible && (
-          <div className="chat-input__emoji-picker">
-            <Picker set="apple" />
+    <Fragment>
+      <div className="chat-input">
+        <div>
+          <div className="chat-input__smile-btn">
+            <div className="chat-input__emoji-picker">
+              {emojiPickerVisible && (
+                <Picker onSelect={emojiTag => addEmoji(emojiTag)} set="apple" />
+              )}
+            </div>
+            <Button onClick={toggleEmojiPicker} type="link" shape="circle" icon="smile" />
+          </div>
+          {isRecording ? (
+            <div className="chat-input__record-status">
+              <i className="chat-input__record-status-bubble"></i>
+              Recording...
+              <Button
+                onClick={onHideRecording}
+                type="link"
+                shape="circle"
+                icon="close"
+                className="stop-recording"
+              />
+            </div>
+          ) : (
+            <TextArea
+              onChange={e => setValue(e.target.value)}
+              onKeyUp={handleSendMessage}
+              size="large"
+              placeholder="Введите текст сообщения…"
+              value={value}
+              autosize={{minRows: 1, maxRows: 6}}
+            />
+          )}
+
+          <div className="chat-input__actions">
+            <UploadField
+              onFiles={onSelectFiles}
+              containerProps={{
+                className: 'chat-input__actions-upload-btn',
+              }}
+              uploadProps={{
+                accept: '.jpg,.jpeg,.png,.gif,.bmp',
+                multiple: 'multiple',
+              }}
+            >
+              <Button type="link" shape="circle" icon="camera" />
+            </UploadField>
+            {isLoading ? (
+              <Button type="link" shape="circle" icon="loading" />
+            ) : isRecording || value || attachments.length ? (
+              <Button onClick={sendMessage} type="link" shape="circle" icon="check-circle" />
+            ) : (
+              <div className="chat-input__record-btn">
+                <Button onClick={onRecord} type="link" shape="circle" icon="audio" />
+              </div>
+            )}
+          </div>
+        </div>
+        {attachments.length > 0 && (
+          <div className="chat-input__attachments">
+            <UploadFiles removeAttachment={removeAttachment} attachments={attachments} />
           </div>
         )}
-
-        <Button onClick={toggleEmojiPicker} shape="circle" icon={<SmileOutlined />} />
       </div>
-      <Input
-        onChange={e => setValue(e.target.value)}
-        size="large"
-        placeholder="Введите текст сообщения"
-      />
-      <div className="chat-input__actions">
-        <UploadField
-          onFiles={files => {
-            console.log(files);
-          }}
-          containerProps={{
-            className: 'chat-input__actions-upload-btn',
-          }}
-          uploadProps={{
-            accept: '.jpg,.jpeg,.png,.gif,.bmp',
-            multiple: 'multiple',
-          }}
-        >
-          <Button shape="circle" icon={<CameraOutlined />} />
-        </UploadField>
-        {value ? (
-          <Button shape="circle" icon={<SendOutlined />} />
-        ) : (
-          <Button shape="circle" icon={<AudioOutlined />} />
-        )}
-      </div>
-    </div>
+    </Fragment>
   );
 };
 
