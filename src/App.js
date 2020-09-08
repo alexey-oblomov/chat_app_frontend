832fb456b054cd64b500bf0e290fbba4a720efac
Alexey Oblomov <alexey.oblomov.1981@gmail.com>
2020-09-08 11:45:25 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {Route, Redirect, Switch} from 'react-router-dom';
 
 import {Auth, Home} from 'pages';
 
-const App = () => (
-  <div className="wrapper">
-    <Route exact path={['/', '/login', '/register']} component={Auth} />
-    <Route exact path="/im" component={Home} />
-  </div>
-);
+const App = props => {
+  const {isAuth} = props;
+  return (
+    <div className="wrapper">
+      <Switch>
+        <Route exact path={['/signin', '/signup', '/signup/verify']} component={Auth} />
+        <Route path="/" render={() => (isAuth ? <Home /> : <Redirect to="/signin" />)} />
+      </Switch>
+    </div>
+  );
+};
 
-export default App;
+export default connect(({user}) => ({isAuth: user.isAuth}))(App);
